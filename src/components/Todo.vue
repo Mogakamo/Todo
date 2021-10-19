a328af4d6f15e675c3f070ec72aa55027443b0d7
Mogakamo <mogaka.amo254@gmail.com>
2021-10-19 05:49:49 +0000

    refactored the whole project and now I am starting from scratch

---
+++ src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
