03f0a40db9463a64e260812630b5147208a16263
Mogakamo <mogaka.amo254@gmail.com>
2021-10-21 09:05:04 +0000

    The login route is working well

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div class="auth-wrapper"><h1>Login</h1></div>
 </template>
 
 <script>
@@ -8,4 +8,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="css" scoped>
+.auth-wrapper {
+  color: white;
+}
+</style>
