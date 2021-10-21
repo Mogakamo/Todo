9b7395ab539ce5b68fc4360e5265463f97ba7fbb
Mogakamo <mogaka.amo254@gmail.com>
2021-10-21 08:53:24 +0000

    firebase configured successfully

--- src/main.js
+++ src/main.js
@@ -4,4 +4,22 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+//firebase imports
+import firebase from "firebase/compat/app";
+import { firebaseConfig } from "./firebase/firebase-config";
+import "firebase/compat/firestore";
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+//exporting the db object from firestore
+export const db = firebase.firestore();
+
+db.collection("messages")
+  .get()
+  .then((r) => {
+    r.docs.map((doc) => {
+      console.log(doc.data());
+    });
+  });
 createApp(App).use(store).use(router).mount("#app");
