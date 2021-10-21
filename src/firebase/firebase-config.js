96d5b1a7620705d0b7812fea42d7b54ff0052b3b
Mogakamo <mogaka.amo254@gmail.com>
2021-10-21 08:46:40 +0000

    added the firebase-config details

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBesLCbhM0Y8CcLlLQCYoC61HYcyv0eMrA",
+
+  authDomain: "todo-app-5caa5.firebaseapp.com",
+
+  projectId: "todo-app-5caa5",
+
+  storageBucket: "todo-app-5caa5.appspot.com",
+
+  messagingSenderId: "309296333351",
+
+  appId: "1:309296333351:web:978c1068ff854ca9018402",
+
+  measurementId: "G-Q6N8XYT34T",
+};
