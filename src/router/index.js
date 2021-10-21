65ad4fb45223f18918b422fe4c24510100afe143
Mogakamo <mogaka.amo254@gmail.com>
2021-10-21 09:54:46 +0000

    login and registration is working well

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Register from "../views/Register.vue";
+//import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      authRequired: true,
+    },
   },
   {
     path: "/register",
@@ -25,5 +30,21 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some((record) => record.meta.authRequired)) {
+//     if (firebase.auth().currentUser) {
+//       next();
+//     } else {
+//       alert("You must be logged in to see this page");
+//       router.push("/login")
+//         .catch(error => {
+//             alert(error.message)
+//           })
+//     }
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
