1993235e02e37262a14cf896c128a6b4c969797c
Mogakamo <mogaka.amo254@gmail.com>
2021-10-21 09:21:39 +0000

    registration is successfull

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="auth-wrapper">
-    <form @submit.prevent="login">
+    <form @submit.prevent="register">
       <h1>Register</h1>
       <input
         type="text"
@@ -20,8 +20,31 @@
 </template>
 
 <script>
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
 export default {
   name: "Authentication",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          alert("Successfully registered. Please proceed to login!");
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+  },
 };
 </script>
 
